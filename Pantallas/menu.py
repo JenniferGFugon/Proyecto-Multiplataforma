a6897f18a38aea158f39103c70eff09e74cd6e9d
JenniferGFugon <jgurrerofugon500@gmail.com>
2020-04-03 14:09:59 -0600

    hacer que el boton inventario llame a la pantalla correspondiente

--- Pantallas/menu.py
+++ Pantallas/menu.py
@@ -8,6 +8,7 @@ import sqlite3
 from sqlite3 import Error
 from PIL import Image
 
+
 class Menu(QWidget):
     """ Ventana del menu del sistema. """
 
@@ -19,8 +20,9 @@ class Menu(QWidget):
         self.setPalette(paleta)
         self.setWindowTitle("Menu")
         self.setGeometry(450, 150, 590, 285)
-
         self.UI()
+       
+
         
     def UI(self):
         """ Cargar lo que es el diseño de la ventana """
@@ -51,13 +53,20 @@ class Menu(QWidget):
 
    
     def buttons(self):
+
         """ Botones que conforman la ventana de menu"""
+        
+    
         btn_inventario = QPushButton("INVENTARIO", self)
         btn_inventario.setFixedWidth(115)
         btn_inventario.setFixedHeight(50)
         btn_inventario.move(40, 135)
         btn_inventario.setStyleSheet("background-color: rgb(0, 0, 0);\n"
                                          "color: \'white\';")
+        btn_inventario.clicked.connect(self.UI2)
+        btn_inventario.show();
+        
+
         btn_clientes = QPushButton("CLIENTES", self)
         btn_clientes.setFixedWidth(115)
         btn_clientes.setFixedHeight(50)
@@ -79,6 +88,104 @@ class Menu(QWidget):
         btn_empleados.setStyleSheet("background-color: rgb(0, 0, 0);\n"
                                          "color: \'white\';")
 
+    
+    def UI2(self):
+        """ Componentes del diseño de la ventana Inventario """
+       
+        fuente = QFont()
+        fuente.setPointSize(10)
+        fuente.setFamily("Bahnschrift Light")
+        fuente.setBold(True)
+        app.setFont(fuente)  
+        self.setWindowTitle("Inventario")
+        self.setGeometry(430, 110, 700, 600)
+        
+        self.encabezado()
+        self.botones()
+        self.layouts()
+        self.show()
+        #sys.exit(app.exec_())
+
+
+    def encabezado(self):
+        """ Encabezado de la ventana """
+        paleta = QPalette()
+        paleta.setColor(QPalette.Background, QColor(0, 0, 0))
+
+        frame = QFrame(self)
+        frame.setFrameShape(QFrame.NoFrame)
+        frame.setFrameShadow(QFrame.Sunken)
+        frame.setAutoFillBackground(True)
+        frame.setPalette(paleta)
+        frame.setFixedWidth(2000)
+        frame.setFixedHeight(74)
+        frame.move(0, 0)
+
+        logo = QLabel(frame)
+        logo.setFixedWidth(90)
+        logo.setFixedHeight(50)
+        logo.setPixmap(QPixmap("Logo.png").scaled(90, 90, Qt.KeepAspectRatio,
+                                                         Qt.SmoothTransformation))
+        logo.move(560, 1)
+
+        font = QFont()
+        font.setPointSize(19)
+        font.setBold(True) 
+
+        lbl_titulo = QLabel("<font color='white'>Inventario</font>", frame)
+        lbl_titulo.setFont(font)
+        lbl_titulo.move(545, 40)
+
+    def botones(self):
+        """ Botones que conforman la ventana de inventario """
+        self.lista_inventario = QListWidget()
+        self.btn_modificar_inventario = QPushButton("Modificar Producto")
+        self.btn_modificar_inventario.setStyleSheet("background-color: rgb(0, 0, 0);\n"
+                                         "color: \'white\';")
+        self.btn_modificar_inventario.setFixedWidth(165)
+        self.btn_modificar_inventario.setFixedHeight(40)
+
+        self.btn_eliminar_inventario = QPushButton("Eliminar Producto")
+        self.btn_eliminar_inventario.setStyleSheet("background-color: rgb(0, 0, 0);\n"
+                                         "color: \'white\';")
+        self.btn_eliminar_inventario.setFixedWidth(165)
+        self.btn_eliminar_inventario.setFixedHeight(40)
+
+        self.btn_menu = QPushButton("Menu Principal")
+        self.btn_menu.setStyleSheet("background-color: rgb(0, 0, 0);\n"
+                                         "color: \'white\';")
+        #self.btn_menu.clicked.connect(self.llamar_menu)
+        self.btn_menu.setFixedWidth(165)
+        self.btn_menu.setFixedHeight(40)
+
+    def layouts(self):
+        """ Layouts que componen la ventana """
+        # Layouts
+        self.main_layout = QHBoxLayout()
+        self.left_layout = QFormLayout()
+        self.right_main_layout = QVBoxLayout()
+        self.right_top_layout = QHBoxLayout()
+        self.right_bottom_layout = QHBoxLayout()
+        self.top_layout = QVBoxLayout()
+
+        self.right_main_layout.addLayout(self.right_top_layout)
+        self.right_main_layout.addLayout(self.right_bottom_layout)
+        self.main_layout.addLayout(self.left_layout, 70)
+        self.main_layout.addLayout(self.right_main_layout, 30)
+        self.main_layout.addLayout(self.top_layout)
+       
+        self.left_layout.addWidget(self.lista_inventario)
+        self.right_top_layout.addWidget(self.btn_modificar_inventario)
+        self.right_main_layout.addWidget(self.btn_menu)
+        self.right_bottom_layout.addWidget(self.btn_eliminar_inventario)
+        self.top_layout.addWidget(self.encabezado())
+
+        self.setLayout(self.main_layout)
+        
+  
+
+
+
 if __name__ == "__main__":
     
     app = QApplication(sys.argv)
@@ -92,5 +199,6 @@ if __name__ == "__main__":
     window = Menu()
     window.show()
     sys.exit(app.exec_())
+           
            
     
