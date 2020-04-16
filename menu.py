1ed8d0e2d3233cda99f8523f1fdb1aa245170b00
Diegomau182 <61211670+Diegomau182@users.noreply.github.com>
2020-04-16 00:48:35 -0600

    llama y guerda productos

--- menu.py
+++ menu.py
@@ -122,8 +122,6 @@ class Menu(QWidget):
         self.cliente = VentanaCliente()
         self.close()          
 
-
-
     #------------PANTALLA INVENTARIO---------------------
 class VentanaInventario(QWidget):
     """ Ventana de listado de inventario del sistema """
@@ -181,6 +179,14 @@ class VentanaInventario(QWidget):
     def botonesInventario(self):
         """ Botones que conforman la ventana de inventario """
         self.lista_inventario = QListWidget()
+        self.btn_agregar_inventario = QPushButton("Agregar Producto")
+        self.btn_agregar_inventario.setStyleSheet("background-color: rgb(0, 0, 0);\n"
+                                         "color: \'white\';")
+        self.btn_agregar_inventario.clicked.connect(self.Llamar_agregar)
+
+        self.btn_agregar_inventario.setFixedWidth(165)
+        self.btn_agregar_inventario.setFixedHeight(40)
+        
         self.btn_modificar_inventario = QPushButton("Modificar Producto")
         self.btn_modificar_inventario.setStyleSheet("background-color: rgb(0, 0, 0);\n"
                                          "color: \'white\';")
@@ -219,18 +225,280 @@ class VentanaInventario(QWidget):
         self.main_layout.addLayout(self.top_layout)
        
         self.left_layout.addWidget(self.lista_inventario)
-        self.right_top_layout.addWidget(self.btn_modificar_inventario)
+        self.right_top_layout.addWidget(self.btn_agregar_inventario)
+        self.right_bottom_layout.addWidget(self.btn_modificar_inventario)
+        self.right_main_layout.addWidget(self.btn_eliminar_inventario)
+        self.btn_menu.move(415, 400)
         self.right_main_layout.addWidget(self.btn_menu)
-        self.right_bottom_layout.addWidget(self.btn_eliminar_inventario)
         self.top_layout.addWidget(self.encabezadoInventario())
         self.setLayout(self.main_layout)
 
 
+    def Llamar_agregar(self):
+        self.llamar_agregar = AgregarProducto()
+        self.close()
+
     def llamar_menu(self):
         self.call = Menu()
-        self.close()    
+        self.close()
+
+   
+
     #------------------------------Pantalla Agregar Inventario---------------------------------
+class AgregarProducto(QWidget):
+    """ Ventana para agregar nuevos productos """
+
+    def __init__(self):
+        super().__init__()
+        self.basedatos = VentaDB("base_UMA.db")
+        paleta = QPalette()
+        paleta.setColor(QPalette.Background, QColor(229, 25, 25))
+        self.setPalette(paleta)
+        self.setWindowTitle("Agregar Producto")
+        self.setGeometry(430, 120, 750, 550)
+        self.UI()
+        self.show()
+        
+    def UI(self):
+        """ Componentes del diseño de la ventana """
+        self.encabezado()
+        self.widgets()
+        self.botones() 
+
+    def encabezado(self):
+        """ Encabezado de la ventana """
+        paleta = QPalette()
+        paleta.setColor(QPalette.Background, QColor(0, 0, 0))
+
+        frame = QFrame(self)
+        frame.setFrameShape(QFrame.NoFrame)
+        frame.setFrameShadow(QFrame.Sunken)
+        frame.setAutoFillBackground(True)
+        frame.setPalette(paleta)
+        frame.setFixedWidth(2000)
+        frame.setFixedHeight(84)
+        frame.move(0, 0)
+
+        logo = QLabel(frame)
+        logo.setFixedWidth(90)
+        logo.setFixedHeight(50)
+        logo.setPixmap(QPixmap("Logo.png").scaled(90, 90, Qt.KeepAspectRatio,
+                                                         Qt.SmoothTransformation))
+        logo.move(20, 12)
+
+        fuenteTitulo = QFont()
+        fuenteTitulo.setPointSize(19)
+        fuenteTitulo.setBold(True) 
+
+        self.lbl_Titulo = QLabel("<font color='white'>Agregar Producto</font>", frame)
+        self.lbl_Titulo.setFont(fuenteTitulo)
+        self.lbl_Titulo.move(270, 25)
+
+    def widgets(self):
+        """ Widgets que conforman la ventana de agregar un producto """
+        self.fuente = QFont()
+        self.fuente.setPointSize(10)
+        self.fuente.setFamily("Bahnschrift Light")
+        self.fuente.setBold(True)
+        
+        self.lbl_nombre_producto = QLabel("Nombre del Producto: ", self)
+        self.lbl_nombre_producto.setFont(fuente)
+        self.lbl_nombre_producto.move(60, 140)
+        self.input_nombre_producto = QLineEdit(self)
+        self.input_nombre_producto.move(225, 140)
+        self.input_nombre_producto.setFixedWidth(400)
+        self.lbl_categoria_producto = QLabel("Categoria: ", self)
+        self.lbl_categoria_producto.setFont(fuente)
+        self.lbl_categoria_producto.move(60, 180)
+        self.input_categoria_producto = QLineEdit(self)
+        self.input_categoria_producto.move(225, 180)
+        self.input_categoria_producto.setFixedWidth(400)
+        self.lbl_existencia_producto = QLabel("Existencia: ", self)
+        self.lbl_existencia_producto.setFont(fuente)
+        self.lbl_existencia_producto.move(60, 220)
+        self.input_existencia_producto = QLineEdit(self)
+        self.input_existencia_producto.move(225, 220)
+        self.input_existencia_producto.setFixedWidth(400)  
+        self.lbl_precio_compra = QLabel("Precio de Compra: ", self)
+        self.lbl_precio_compra.setFont(fuente)
+        self.lbl_precio_compra.move(60, 260)
+        self.input_precio_compra = QLineEdit(self)
+        self.input_precio_compra.move(225, 260)
+        self.input_precio_compra.setFixedWidth(400)
+        self.lbl_precio_venta = QLabel("Precio de venta: ", self)
+        self.lbl_precio_venta.setFont(fuente)
+        self.lbl_precio_venta.move(60, 300)
+        self.input_precio_venta = QLineEdit(self)
+        self.input_precio_venta.move(225, 300)
+        self.input_precio_venta.setFixedWidth(400)  
+
+    def botones(self):
+        """ Botones que conforman la ventana de agregar un producto """
+        self.fuente = QFont()
+        self.fuente.setPointSize(10)
+        self.fuente.setFamily("Bahnschrift Light")
+        self.fuente.setBold(True)
+
+        self.btn_guardar_producto = QPushButton("Guardar", self)
+        self.btn_guardar_producto.setFixedWidth(135)
+        self.btn_guardar_producto.setFixedHeight(28)
+        self.btn_guardar_producto.move(215, 400)
+        self.btn_guardar_producto.setStyleSheet("background-color: rgb(0, 0, 0);\n"
+                                        "color: \'white\';")
+        self.btn_guardar_producto.clicked.connect(self.insertar_inventario)                                 
+        self.btn_guardar_producto.setFont(fuente)
+        self.btn_menu = QPushButton("Menu Principal", self)
+        self.btn_menu.setFixedWidth(135)
+        self.btn_menu.setFixedHeight(28)
+        self.btn_menu.move(415, 400)
+        self.btn_menu.setStyleSheet("background-color: rgb(0, 0, 0);\n"
+                                         "color: \'white\';")
+        self.btn_menu.setFont(fuente)
     
+    def insertar_inventario(self):
+        """ Insertar los valores del formulario a la tabla de empleado """
+        # Verificar si los valores requeridos fueron agregados
+        if (self.input_nombre_producto.text() or self.input_categoria_producto.text() 
+            or self.input_existencia_producto.text() or self.input_precio_compra.text() or
+            self.input_precio_venta.text() != ""):
+            inventario = (str(self.input_nombre_producto.text()), str(self.input_categoria_producto.text()),
+                        str(self.input_existencia_producto.text()) , str(self.input_precio_compra.text()),
+                        str(self.input_precio_venta.text()) )
+
+            try:
+                self.basedatos.add_producto(inventario)
+                QMessageBox.information(
+                    self, "Guardar", "Producto agregado correctamente")
+                self.close()
+                
+                self.Llamar_agregar()
+            except Error as e:
+                QMessageBox.information(
+                    self, "Error", "Error al momento de agregar el producto")
+        else:
+            QMessageBox.information(
+                self, "Advertencia", "Debes ingresar toda la información")    
+
+
+#------------------------------------------------------------------------------------------------------------
+class AgregarCliente(QWidget):
+    """ Ventana para agregar nuevos clientes """
+
+    def __init__(self):
+        super().__init__()
+        paleta = QPalette()
+        paleta.setColor(QPalette.Background, QColor(229, 25, 25))
+        self.setPalette(paleta)
+        self.setWindowTitle("Agregar un cliente")
+        self.setGeometry(430, 120, 750, 650)
+        self.UI()
+        
+    def UI(self):
+        """ Componentes del diseño de la ventana """
+        self.encabezado()
+        self.labels()
+        self.botones()
+
+    def encabezado(self):
+        """ Encabezado de la ventana """
+        paleta = QPalette()
+        paleta.setColor(QPalette.Background, QColor(0, 0, 0))
+
+        frame = QFrame(self)
+        frame.setFrameShape(QFrame.NoFrame)
+        frame.setFrameShadow(QFrame.Sunken)
+        frame.setAutoFillBackground(True)
+        frame.setPalette(paleta)
+        frame.setFixedWidth(2000)
+        frame.setFixedHeight(84)
+        frame.move(0, 0)
+
+        logo = QLabel(frame)
+        logo.setFixedWidth(90)
+        logo.setFixedHeight(50)
+        logo.setPixmap(QPixmap("Logo.png").scaled(90, 90, Qt.KeepAspectRatio,
+                                                         Qt.SmoothTransformation))
+        logo.move(20, 12)
+
+        fuenteTitulo = QFont()
+        fuenteTitulo.setPointSize(19)
+        fuenteTitulo.setBold(True) 
+
+        lbl_Titulo = QLabel("<font color='white'>Agregar Cliente</font>", frame)
+        lbl_Titulo.setFont(fuenteTitulo)
+        lbl_Titulo.move(280, 25)
+
+    def labels(self):
+        """ Botones que conforman la ventana de agregar cliente """
+        fuente = QFont()
+        fuente.setPointSize(10)
+        fuente.setFamily("Bahnschrift Light")
+        fuente.setBold(True)
+
+        lbl_id_cliente = QLabel("Identidad: ", self)
+        lbl_id_cliente.setFont(fuente)
+        lbl_id_cliente.move(60, 140)
+        input_id_cliente = QLineEdit(self)
+        input_id_cliente.move(250, 140)
+        input_id_cliente.setFixedWidth(400)
+        lbl_nombre_cliente = QLabel("Nombre del cliente: ", self)
+        lbl_nombre_cliente.setFont(fuente)
+        lbl_nombre_cliente.move(60, 180)
+        input_nombre_cliente = QLineEdit(self)
+        input_nombre_cliente.move(250, 180)
+        input_nombre_cliente.setFixedWidth(400)
+        lbl_telefono_cliente = QLabel("Telefono: ", self)
+        lbl_telefono_cliente.setFont(fuente)
+        lbl_telefono_cliente.move(60, 220)
+        input_telefono_cliente = QLineEdit(self)
+        input_telefono_cliente.move(250, 220)
+        input_telefono_cliente.setFixedWidth(400)
+        lbl_celular_cliente = QLabel("Celular: ", self)
+        lbl_celular_cliente.setFont(fuente)
+        lbl_celular_cliente.move(60, 260)
+        input_celular_cliente = QLineEdit(self)
+        input_celular_cliente.move(250, 260)
+        input_celular_cliente.setFixedWidth(400)  
+        lbl_rtn_cliente = QLabel("RTN: ", self)
+        lbl_rtn_cliente.setFont(fuente)
+        lbl_rtn_cliente.move(60, 300)
+        input_rtn_cliente = QLineEdit(self)
+        input_rtn_cliente.move(250, 300)
+        input_rtn_cliente.setFixedWidth(400)
+        lbl_direccion_cliente = QLabel("Direccion: ", self)
+        lbl_direccion_cliente.setFont(fuente)
+        lbl_direccion_cliente.move(60, 340)
+        input_direccion_cliente = QLineEdit(self)
+        input_direccion_cliente.move(250, 340)
+        input_direccion_cliente.setFixedWidth(400)
+        lbl_correo_cliente = QLabel("Correo Electronico: ", self)
+        lbl_correo_cliente.setFont(fuente)
+        lbl_correo_cliente.move(60, 380)
+        input_correo_cliente = QLineEdit(self)
+        input_correo_cliente.move(250, 380)
+        input_correo_cliente.setFixedWidth(400)
+
+    def botones(self):
+        """ Botones que conforman la ventana de agregar cliente """
+        fuente = QFont()
+        fuente.setPointSize(10)
+        fuente.setFamily("Bahnschrift Light")
+        fuente.setBold(True)
+
+        btn_guardar_cliente = QPushButton("Guardar", self)
+        btn_guardar_cliente.setFixedWidth(135)
+        btn_guardar_cliente.setFixedHeight(28)
+        btn_guardar_cliente.move(215, 510)
+        btn_guardar_cliente.setStyleSheet("background-color: rgb(0, 0, 0);\n"
+                                         "color: \'white\';")
+        btn_guardar_cliente.setFont(fuente)
+        btn_menu = QPushButton("Menu Principal", self)
+        btn_menu.setFixedWidth(135)
+        btn_menu.setFixedHeight(28)
+        btn_menu.move(415, 510)
+        btn_menu.setStyleSheet("background-color: rgb(0, 0, 0);\n"
+                                         "color: \'white\';")
+        btn_menu.setFont(fuente)
 
     #------------------------------------------------------------------------------------------
 
